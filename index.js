b4630ca648b44209f14c6eefed1654a3683622b6
bleufox <thebleufox8@gmail.com>
2021-09-23 00:05:54 -0700

    Added index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const session = require('express-session');
+const router = require('./routes');
+const sequelize = require('./config/connection');
+const { Sequelize } = require('sequelize');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Express App Setup
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+app.use(router);
+app.use(express.static('public'));
+
+server.listen(PORT, function() {
+  console.log('Trails App server running');
+});
